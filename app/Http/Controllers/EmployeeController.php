02512d9e84e2ec43641259a0db4ab66ef2c4f9c8
Kriti M <macbookpro@macbooks-MacBook-Pro.local>
2023-05-24 21:56:15 +0700

    delete model Govanda, delete controller Govanda and move the code to Employee namespace

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    public function index(){
+
+        $data = Employee::all();
+        return view('datapegawai',compact('data'));
+    }
+
+    public function tambahpegawai(){
+        return view('tambahdata');
+    }
+    public function insertdata(Request $request){
+       // dd($request->all());
+        Employee::create($request->all());
+        return redirect()->route('pegawai')->with('success','data berhasil di tambahkan');
+
+    }
+
+    public function tampilkandata($id){
+      $data = Employee::find($id);
+      //dd($data);
+      return view('tampildata', compact('data'));
+    }
+
+    public function updatedata(Request $request, $id){
+        $data = Employee::find($id);
+        $data->update($request->all());
+        return redirect()->route('pegawai')->with('success','data berhasil di update');
+    }
+
+    public function delete ($id){
+       $data = Employee::find($id);
+       $data->delete();
+       return redirect()->route('pegawai')->with('success','data berhasil di delete');
+    }
 }
